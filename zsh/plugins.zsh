568bf842a21c55be3e566de012467cfa8f759807
Yaroslav.Dusaniuk <yaroslav.dusaniuk@gmail.com>
2020-12-22 14:37:13 +0200

    chore(zsh): configure plugins via antigen

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,15 +1,14 @@
 # use antigen plugin manager
 source $HOME/friday/deps/antigen/antigen.zsh
 
-# Which plugins would you like to load?
-# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
-
 antigen use oh-my-zsh
 
+# add plugins from the default repo (oh-my-zsh)
+antigen bundle git
+antigen bundle heroku
+antigen bundle command-not-found
+
+# cool spaceship theme for zsh
 antigen theme denysdovhan/spaceship-prompt
 
 antigen apply
