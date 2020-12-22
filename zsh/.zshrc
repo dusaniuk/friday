42e4d44a8e9b01dbf689e45f4228016f2b392988
Yaroslav.Dusaniuk <yaroslav.dusaniuk@gmail.com>
2020-12-22 06:58:23 +0200

    feat(zsh): link configuration

---
+++ zsh/.zshrc
@@ -0,0 +1,9 @@
+# welcome me
+figlet "Welcome back, Mr Yaro" | lolcat
+
+source "$HOME/friday/zsh/aliases.zsh"
+source "$HOME/friday/zsh/plugins.zsh"
+source "$HOME/friday/zsh/exports.zsh"
+
+# oh my zsh source should be included after all env variables are set up
+# probably there's a way to avoid that, gotta look into that
---
+++ zsh/exports.zsh
@@ -0,0 +1,17 @@
+# use nvm
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="amuse"
+
+# Path to your oh-my-zsh installation.
+export ZSH="/Users/dusaniuk/.oh-my-zsh"
+source $ZSH/oh-my-zsh.sh
+
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:/usr/local/bin:$PATH
